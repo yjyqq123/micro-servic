5345b174ee2040e849c7fc251e72b6cdd6e60d87
yjyqq123 <2641617428@qq.com>
2020-09-20 19:51:21 +0800

    micro-servic

---
+++ user-center/src/main/java/com/soft1851/usercenter/UserCenterApplication.java
@@ -0,0 +1,15 @@
+package com.soft1851.usercenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+@MapperScan(basePackages = "com.soft1851.usercenter.dao")
+public class UserCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserCenterApplication.class, args);
+    }
+
+}
